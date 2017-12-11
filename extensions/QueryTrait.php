e7e8e4b04c7defe0f4569a675b6c9d9fc84ca561
hubeiwei <hubeiwei@hotmail.com>
2017-12-11 18:58:48 +0800

    增加查询缓存

--- extensions/QueryTrait.php
+++ extensions/QueryTrait.php
@@ -6,7 +6,52 @@ use hubeiwei\yii2tools\helpers\Helper;
 
 trait QueryTrait
 {
+    /**
+     * @var string 时间分隔符
+     */
     public $timeRangeSeparator = '-';
+
+    /**
+     * 缓存时间，单位：秒，用0来表示永久缓存，用负数来表示不使用缓存
+     * @var int
+     * @see cache()
+     */
+    public $queryCacheDuration;
+    /**
+     * 缓存依赖
+     * @var \yii\caching\Dependency
+     * @see cache()
+     */
+    public $queryCacheDependency;
+
+    /**
+     * Enables query cache for this command.
+     * @param int $duration 缓存时间，单位：秒，用0来表示永久缓存，用负数来表示不使用缓存
+     * @param \yii\caching\Dependency $dependency 缓存依赖
+     * @return $this
+     */
+    public function cache($duration = null, $dependency = null)
+    {
+        $this->queryCacheDuration = $duration;
+        $this->queryCacheDependency = $dependency;
+        return $this;
+    }
+
+    /**
+     * Creates a DB command that can be used to execute this query.
+     * @param \yii\db\Connection $db the database connection used to generate the SQL statement.
+     * If this parameter is not given, the `db` application component will be used.
+     * @return \yii\db\Command the created DB command instance.
+     */
+    public function createCommand($db = null)
+    {
+        $command = parent::createCommand($db);
+        if ($this->queryCacheDuration !== null || $this->queryCacheDependency !== null) {
+            $command->queryCacheDuration = $this->queryCacheDuration === null ? $db->queryCacheDuration : $this->queryCacheDuration;
+            $command->queryCacheDependency = $this->queryCacheDependency;
+        }
+        return $command;
+    }
 
     /**
      * @param string $attribute
@@ -32,7 +77,7 @@ trait QueryTrait
 
             if ($value === '') {
                 continue;
-//                return $this;
+                // return $this;
             }
 
             if ($op === '') {
