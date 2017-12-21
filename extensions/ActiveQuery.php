58b8af4d8988d3388a20867fcbcb2c6963f3c7fe
hubeiwei <hubeiwei@hotmail.com>
2017-12-21 14:24:13 +0800

    queryScalar重新实例化了Query，需要重新配置缓存

--- extensions/ActiveQuery.php
+++ extensions/ActiveQuery.php
@@ -26,4 +26,29 @@ class ActiveQuery extends YiiActiveQuery
         }
         return $command;
     }
+
+    /**
+     * @inheritdoc
+     */
+    protected function queryScalar($selectExpression, $db)
+    {
+        /* @var $modelClass ActiveRecord */
+        $modelClass = $this->modelClass;
+        if ($db === null) {
+            $db = $modelClass::getDb();
+        }
+
+        if ($this->sql === null) {
+            return parent::queryScalar($selectExpression, $db);
+        }
+
+        $command = (new Query())->select([$selectExpression])
+            ->from(['c' => "({$this->sql})"])
+            ->params($this->params)
+            ->createCommand($db);
+        if ($this->queryCacheDuration !== null || $this->queryCacheDependency !== null) {
+            $command->cache($this->queryCacheDuration, $this->queryCacheDependency);
+        }
+        return $command->queryScalar();
+    }
 }
